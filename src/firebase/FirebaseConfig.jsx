6184fb24d99ac0455d1a539618ca3f84ba8347b8
Kushal <kushaldutta432@gmail.com>
2024-12-30 13:25:49 +0530

    second

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -2,20 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBQsJ5MS4-MacHnO5wxQX8V2tGe0y6eCXo",
-  authDomain: "myecommerce2-7b8fc.firebaseapp.com",
-  projectId: "myecommerce2-7b8fc",
-  storageBucket: "myecommerce2-7b8fc.appspot.com",
-  messagingSenderId: "715233167432",
-  appId: "1:715233167432:web:3a0acec83b123399d3307d"
+  apiKey: "AIzaSyCwq7j5nWJJnvQTkSBPrCJZ32d5W1jXE_M",
+  authDomain: "ecommerce-7e47a.firebaseapp.com",
+  projectId: "ecommerce-7e47a",
+  storageBucket: "ecommerce-7e47a.firebasestorage.app",
+  messagingSenderId: "400118000532",
+  appId: "1:400118000532:web:a5747dd42e1a147ad17d81"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
 
